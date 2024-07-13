3bad91da573a99e79e5329d7b4a891fd5d200a47
RyoheiTomiyama <tennisnowboarderyou@gmail.com>
2024-07-13 22:09:11 +0900

    chore

--- app/infra/db/model/models.go
+++ app/infra/db/model/models.go
@@ -8,12 +8,12 @@ import (
 )
 
 type Card struct {
-	ID        int64
-	DeckID    int64
-	Question  string
-	Answer    string
-	CreatedAt time.Time
-	UpdatedAt time.Time
+	ID        int64     `db:"id"`
+	DeckID    int64     `db:"deck_id"`
+	Question  string    `db:"question"`
+	Answer    string    `db:"answer"`
+	CreatedAt time.Time `db:"created_at"`
+	UpdatedAt time.Time `db:"updated_at"`
 }
 
 type Deck struct {
@@ -29,6 +29,17 @@ type User struct {
 	Name      string
 	CreatedAt sql.NullTime
 	UpdatedAt sql.NullTime
+}
+
+func (d *Card) ToDomain() *domain.Card {
+	return &domain.Card{
+		ID:        d.ID,
+		DeckID:    d.DeckID,
+		Question:  d.Question,
+		Answer:    d.Answer,
+		CreateAt:  d.CreatedAt,
+		UpdatedAt: d.UpdatedAt,
+	}
 }
 
 func (d *Deck) ToDomain() *domain.Deck {
