73a8f03ec5a4ca4cbdf5d37d9f96141dd2a6c037
RyoheiTomiyama <tennisnowboarderyou@gmail.com>
2024-07-13 15:28:55 +0900

    test: Decks

--- app/router/graph/resolver/deck.resolvers_test.go
+++ app/router/graph/resolver/deck.resolvers_test.go
@@ -13,6 +13,58 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
+func assertDeck(t *testing.T, expect *domain.Deck, actual *model.Deck) {
+	t.Helper()
+
+	em := &model.Deck{
+		ID:        expect.ID,
+		UserID:    expect.UserID,
+		Name:      expect.Name,
+		CreatedAt: expect.CreateAt,
+		UpdatedAt: expect.UpdatedAt,
+	}
+	assert.Equal(t, em, actual)
+}
+
+func (s *resolverSuite) TestDecks() {
+	userID := "test_user"
+	ctx := context.Background()
+	ctx = auth.New(&domain.User{ID: userID}).WithCtx(ctx)
+
+	fx := fixture.New(s.dbx)
+
+	decks := fx.CreateDeck(s.T(),
+		&fixture.DeckInput{UserID: userID},
+		&fixture.DeckInput{UserID: userID},
+		&fixture.DeckInput{UserID: "other_user"},
+	)
+
+	s.T().Run("Decksが取得できること", func(t *testing.T) {
+		result, err := s.resolver.Query().Decks(ctx)
+		assert.NoError(t, err)
+
+		t.Run("ログインユーザのDeckが取得できていること", func(t *testing.T) {
+			assert.Len(t, result.Decks, 2)
+			assertDeck(t, decks[0].ToDomain(), result.Decks[0])
+			assertDeck(t, decks[1].ToDomain(), result.Decks[1])
+		})
+
+		t.Run("他ユーザのDeckが取得されていないこと", func(t *testing.T) {
+			for _, d := range result.Decks {
+				assert.NotEqual(t, decks[2].ID, d.ID)
+			}
+		})
+	})
+
+	s.T().Run("ログインユーザが他ユーザ所有のDeckを取得しようとした場合", func(t *testing.T) {
+		decks := fx.CreateDeck(t, &fixture.DeckInput{})
+
+		deck, err := s.resolver.Query().Deck(ctx, decks[0].ID)
+		assert.Nil(t, deck)
+		assertion.AssertError(t, "取得する権限がありません", errutil.CodeBadRequest, err)
+	})
+}
+
 func (s *resolverSuite) TestDeck() {
 	userID := "test_user"
 	ctx := context.Background()
@@ -26,12 +78,7 @@ func (s *resolverSuite) TestDeck() {
 		deck, err := s.resolver.Query().Deck(ctx, decks[0].ID)
 		assert.NoError(t, err)
 
-		var m model.Deck
-		if err = model.FromDomain(ctx, decks[0].ToDomain(), &m); err != nil {
-			t.Fatal(err)
-		}
-
-		assert.Equal(t, m, *deck)
+		assertDeck(t, decks[0].ToDomain(), deck)
 	})
 
 	s.T().Run("ログインユーザが他ユーザ所有のDeckを取得しようとした場合", func(t *testing.T) {
