93e099bc1a1cdec88636992315205069d98988de
RyoheiTomiyama <tennisnowboarderyou@gmail.com>
2024-07-25 23:05:51 +0900

    いい感じの反復期間にできた

--- app/service/card/card.go
+++ app/service/card/card.go
@@ -1,18 +1,31 @@
 package service
 
+import "math"
+
 type cardService struct{}
 
-type ICardService interface{}
+type ICardService interface {
+	CalcSchedule(grade int, interval int, factor float64) (nextInterval int, nextFactor float64)
+}
 
 func NewCardService() ICardService {
 	return &cardService{}
 }
 
-func (s *cardService) CalcSchedule(grade int, interval, factor float64) (nextInterval, nextFactor float64) {
-	factor += 0.1 - float64(5-grade)*(0.08+float64(5-grade)*0.02)
-	factor = max(1.3, factor)
+func (s *cardService) CalcSchedule(grade int, interval int, factor float64) (nextInterval int, nextFactor float64) {
+	factor += (0.2 - float64(5-grade)*(0.08+float64(5-grade)*0.02)) / (factor * factor * factor)
+	factor = round(max(1.0, factor), 2)
 
-	interval = interval * factor
+	interval = int(float64(interval) * float64(grade-2) * factor)
 
 	return interval, factor
+}
+
+// pos桁数で四捨五入
+func round(num float64, pos int) float64 {
+	shift := math.Pow10(pos)             // 小数の位置をずらすためのシフト値を算出
+	shiftedNum := num * shift            // 四捨五入したい桁を小数第一位にずらす
+	roundedNum := math.Round(shiftedNum) // 小数第一位を四捨五入する
+	result := roundedNum / shift         // 小数の位置を元に戻す
+	return result
 }
