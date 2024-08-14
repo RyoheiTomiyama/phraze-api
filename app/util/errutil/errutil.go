96dae0bc72519042f65f33ae1d852ebc1036a0d0
RyoheiTomiyama <tennisnowboarderyou@gmail.com>
2024-08-14 23:28:13 +0900

    feat: log出力用にstack traceを整形する

--- app/util/errutil/errutil.go
+++ app/util/errutil/errutil.go
@@ -18,8 +18,8 @@ const MaxStackDepth = 16
 type IError interface {
 	Error() string
 	Message() string
-	Format(s fmt.State, r rune)
-	FormatError(p xerrors.Printer) error
+	// Format(s fmt.State, r rune)
+	// FormatError(p xerrors.Printer) error
 	IsClient() bool
 	Code() int
 	StackTrace() []uintptr
@@ -32,7 +32,7 @@ func New(code ErrorCode, format string, args ...interface{}) IError {
 		original: e,
 		message:  e.Error(),
 		code:     code,
-		stack:    caller(),
+		stack:    caller(0),
 		frame:    xerrors.Caller(1),
 	}
 }
@@ -44,7 +44,7 @@ func Wrap(err error, msg ...string) *customError {
 			original: ce,
 			message:  lo.Ternary(len(msg) > 0, strings.Join(msg, " "), ce.message),
 			code:     ce.code,
-			stack:    caller(),
+			stack:    ce.stack,
 			frame:    xerrors.Caller(1),
 		}
 	}
@@ -53,7 +53,7 @@ func Wrap(err error, msg ...string) *customError {
 		original: err,
 		message:  strings.Join(msg, " "),
 		code:     CodeInternalError,
-		stack:    caller(),
+		stack:    caller(-1),
 		frame:    xerrors.Caller(1),
 	}
 }
@@ -63,11 +63,46 @@ func As(e error, target interface{}) bool {
 }
 
 func ErrorWithStackTrace(err error) string {
+	var ce *customError
+	if errors.As(err, &ce) {
+		frames := extractFrames(ce.stack)
+		traceString := ""
+		for _, f := range frames {
+			file := f.File
+			if strings.HasPrefix(file, "/go/src/app/") {
+				relativeIndex := strings.Index(file, "/go/src/app/")
+				file = file[relativeIndex+8:]
+			}
+			function := f.Function
+			if i := strings.LastIndex(function, "/"); i > 0 {
+				function = function[i+1:]
+			}
+			traceString += fmt.Sprintf("- %s\n  %s:%d\n", function, file, f.Line)
+		}
+		return traceString
+	}
+
 	return fmt.Sprintf("%+v", err)
 }
 
-func caller() []uintptr {
+func caller(skip int) []uintptr {
 	stack := make([]uintptr, MaxStackDepth)
-	length := runtime.Callers(3, stack)
+	length := runtime.Callers(3+skip, stack)
 	return stack[:length]
+}
+
+func extractFrames(pcs []uintptr) []runtime.Frame {
+	var frames = make([]runtime.Frame, 0, len(pcs))
+	callersFrames := runtime.CallersFrames(pcs)
+
+	for {
+		callerFrame, more := callersFrames.Next()
+		frames = append(frames, callerFrame)
+
+		if !more {
+			break
+		}
+	}
+
+	return frames
 }
