02b8d19291a707357d40f9e88a817b70619f8b0e
RyoheiTomiyama <tennisnowboarderyou@gmail.com>
2024-08-31 17:16:14 +0900

    feat: とりあえず実装

--- app/infra/db/has_permission_by_user_id.go
+++ app/infra/db/has_permission_by_user_id.go
@@ -4,8 +4,34 @@ import (
 	"context"
 
 	"github.com/RyoheiTomiyama/phraze-api/domain"
+	"github.com/RyoheiTomiyama/phraze-api/util/errutil"
+	"github.com/jmoiron/sqlx"
 )
 
 func (c *client) HasPermissionByUserID(ctx context.Context, userID string, key domain.PermissionKey) (bool, error) {
-	return false, nil
+	e := c.execerFrom(ctx)
+
+	query := `
+		SELECT CAST(COUNT(users_roles.user_id) as boolean) FROM users_roles
+			JOIN roles ON roles.id = users_roles.role_id
+			JOIN roles_permissions ON roles_permissions.role_id = roles.id
+			JOIN permissions ON permissions.id = roles_permissions.permission_id
+		WHERE users_roles.user_id = :user_id AND permissions.key = :key
+	`
+	arg := map[string]interface{}{
+		"user_id": userID,
+		"key":     key,
+	}
+
+	query, args, err := e.BindNamed(query, arg)
+	if err != nil {
+		return false, errutil.Wrap(err)
+	}
+
+	var exist bool
+	if err = sqlx.SelectContext(ctx, e, &exist, query, args...); err != nil {
+		return false, errutil.Wrap(err)
+	}
+
+	return exist, nil
 }
