f720f515e3239514c26bb771f35228c1366db0f9
RyoheiTomiyama <tennisnowboarderyou@gmail.com>
2024-09-21 17:03:23 +0900

    feat: delete_deck でrelationカラムを削除

--- app/infra/db/delete_deck.go
+++ app/infra/db/delete_deck.go
@@ -2,24 +2,92 @@ package db
 
 import (
 	"context"
+	"fmt"
 
 	"github.com/RyoheiTomiyama/phraze-api/util/errutil"
+	"github.com/jmoiron/sqlx"
+	"github.com/samber/lo"
 )
 
 func (c *client) DeleteDeck(ctx context.Context, id int64) (int64, error) {
-	e := c.execerFrom(ctx)
+	e, err := c.txFrom(ctx)
+	if err != nil {
+		return 0, errutil.Wrap(err)
+	}
 
-	query := "DELETE FROM decks WHERE id=$1"
+	ar := int64(0)
 
+	query := "SELECT id FROM cards WHERE deck_id=$1"
+	var cards []struct {
+		ID int64
+	}
+	if err = sqlx.SelectContext(ctx, e, &cards, query, id); err != nil {
+		return 0, errutil.Wrap(err)
+	}
+
+	if len(cards) > 0 {
+		cardIDs := lo.Map(cards, func(card struct{ ID int64 }, _ int) int64 {
+			return card.ID
+		})
+
+		query = "DELETE FROM card_reviews WHERE card_id IN (?)"
+		query, args, err := sqlx.In(query, cardIDs)
+		if err != nil {
+			return 0, errutil.Wrap(err)
+		}
+		fmt.Println(query, args, err)
+		query = e.Rebind(query)
+		result, err := e.ExecContext(ctx, query, args...)
+		if err != nil {
+			return 0, errutil.Wrap(err)
+		}
+		ra, err := result.RowsAffected()
+		if err != nil {
+			return 0, errutil.Wrap(err)
+		}
+		ar += ra
+
+		query = "DELETE FROM card_schedules WHERE card_id IN (?)"
+		query, args, err = sqlx.In(query, cardIDs)
+		if err != nil {
+			return 0, errutil.Wrap(err)
+		}
+		fmt.Println(query, args, err)
+		query = e.Rebind(query)
+		result, err = e.ExecContext(ctx, query, args...)
+		if err != nil {
+			return 0, errutil.Wrap(err)
+		}
+		ra, err = result.RowsAffected()
+		if err != nil {
+			return 0, errutil.Wrap(err)
+		}
+		ar += ra
+
+		query = "DELETE FROM cards WHERE deck_id=$1"
+		result, err = e.ExecContext(ctx, query, id)
+		if err != nil {
+			return 0, errutil.Wrap(err)
+		}
+		fmt.Println(query, err)
+		ra, err = result.RowsAffected()
+		if err != nil {
+			return 0, errutil.Wrap(err)
+		}
+		ar += ra
+	}
+
+	query = "DELETE FROM decks WHERE id=$1"
 	result, err := e.ExecContext(ctx, query, id)
 	if err != nil {
 		return 0, errutil.Wrap(err)
 	}
-
-	ar, err := result.RowsAffected()
+	fmt.Println(query, err)
+	ra, err := result.RowsAffected()
 	if err != nil {
 		return 0, errutil.Wrap(err)
 	}
+	ar += ra
 
 	return ar, nil
 }
--- app/infra/db/delete_deck_test.go
+++ app/infra/db/delete_deck_test.go
@@ -21,27 +21,99 @@ func TestDeleteDeck(t *testing.T) {
 	}()
 
 	fx := fixture.New(db)
-	decks := fx.CreateDeck(
-		t,
-		&fixture.DeckInput{UserID: lo.ToPtr("own")},
-	)
 
 	t.Run("正常系", func(t *testing.T) {
+		cases := []struct {
+			name    string
+			arrange func() (deckID int64)
+			assert  func(result int64, err error, deckID int64)
+		}{
+			{
+				name: "紐づくカードがない場合",
+				arrange: func() (deckID int64) {
+					decks := fx.CreateDeck(t, &fixture.DeckInput{UserID: lo.ToPtr("own")})
+
+					return decks[0].ID
+				},
+				assert: func(result int64, err error, deckID int64) {
+					assert.NoError(t, err)
+					assert.Equal(t, int64(1), result)
+
+					var c model.Deck
+					err = db.Get(&c, "SELECT * FROM decks WHERE id=$1", deckID)
+					assert.Equal(t, sql.ErrNoRows, err)
+				},
+			},
+			{
+				name: "カードを持っている場合",
+				arrange: func() (deckID int64) {
+					decks := fx.CreateDeck(t, &fixture.DeckInput{UserID: lo.ToPtr("own")})
+					fx.CreateCard(t, decks[0].ID, fixture.CardInput{})
+
+					return decks[0].ID
+				},
+				assert: func(result int64, err error, deckID int64) {
+					assert.NoError(t, err)
+					assert.Equal(t, int64(2), result)
+
+					var c model.Deck
+					err = db.Get(&c, "SELECT * FROM decks WHERE id=$1", deckID)
+					assert.Equal(t, sql.ErrNoRows, err)
+				},
+			},
+			{
+				name: "学習したことあるデッキの場合",
+				arrange: func() (deckID int64) {
+					decks := fx.CreateDeck(t, &fixture.DeckInput{UserID: lo.ToPtr("own")})
+					cards := fx.CreateCard(t, decks[0].ID, fixture.CardInput{})
+					fx.CreateCardSchedule(t, fixture.CardScheduleInput{CardID: cards[0].ID})
+					fx.CreateCardReview(t, fixture.CardReviewInput{CardID: cards[0].ID})
+
+					return decks[0].ID
+				},
+				assert: func(result int64, err error, deckID int64) {
+					assert.NoError(t, err)
+					assert.Equal(t, int64(4), result)
+
+					var c model.Deck
+					err = db.Get(&c, "SELECT * FROM decks WHERE id=$1", deckID)
+					assert.Equal(t, sql.ErrNoRows, err)
+				},
+			},
+		}
+
 		client := NewTestClient(t, db)
 
-		result, err := client.DeleteDeck(context.Background(), decks[0].ID)
-		assert.NoError(t, err)
-		assert.Equal(t, int64(1), result)
+		for _, cs := range cases {
+			id := cs.arrange()
+			var result int64
+			err := client.Tx(context.Background(), func(ctx context.Context) error {
+				r, err := client.DeleteDeck(ctx, id)
+				if err != nil {
+					return err
+				}
+				result = r
 
-		var c model.Deck
-		err = db.Get(&c, "SELECT * FROM decks WHERE id=$1", decks[0].ID)
-		assert.Equal(t, sql.ErrNoRows, err)
+				return nil
+			})
+			cs.assert(result, err, id)
+		}
+
 	})
 
 	t.Run("存在しないデッキを削除しようとした場合", func(t *testing.T) {
 		client := NewTestClient(t, db)
+		var result int64
+		err := client.Tx(context.Background(), func(ctx context.Context) error {
+			r, err := client.DeleteDeck(ctx, -1)
+			if err != nil {
+				return err
+			}
+			result = r
 
-		result, err := client.DeleteDeck(context.Background(), -1)
+			return nil
+		})
+
 		assert.NoError(t, err)
 		assert.Equal(t, int64(0), result)
 	})
