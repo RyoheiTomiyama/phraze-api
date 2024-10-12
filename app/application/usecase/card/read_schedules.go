77ee53893753b15d0d33c6018917c14da2cca697
RyoheiTomiyama <tennisnowboarderyou@gmail.com>
2024-10-12 23:33:32 +0900

    feat: usecaseにつなげる

--- app/application/usecase/card/read_schedules.go
+++ app/application/usecase/card/read_schedules.go
@@ -4,8 +4,14 @@ import (
 	"context"
 
 	"github.com/RyoheiTomiyama/phraze-api/domain"
+	"github.com/RyoheiTomiyama/phraze-api/util/errutil"
 )
 
 func (u *usecase) ReadSchedules(ctx context.Context, cardIDs []int64) (map[int64]*domain.CardSchedule, error) {
-	return nil, nil
+	smap, err := u.dbClient.GetCardSchedulesByCardID(ctx, cardIDs)
+	if err != nil {
+		return nil, errutil.Wrap(err)
+	}
+
+	return smap, nil
 }
