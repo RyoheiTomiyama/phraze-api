453cc820a7411e40fa82edf4ca171992d2b357d0
RyoheiTomiyama <tennisnowboarderyou@gmail.com>
2025-08-09 17:43:03 +0900

    chore: migration check

--- app/infra/db/update_card_by_id.go
+++ app/infra/db/update_card_by_id.go
@@ -7,6 +7,7 @@ import (
 	"github.com/RyoheiTomiyama/phraze-api/domain"
 	"github.com/RyoheiTomiyama/phraze-api/infra/db/model"
 	"github.com/RyoheiTomiyama/phraze-api/util/errutil"
+	"github.com/RyoheiTomiyama/phraze-api/util/logger"
 )
 
 func (c *client) UpdateCardByID(ctx context.Context, id int64, input *domain.UpdateCardInput) (*domain.Card, error) {
@@ -35,8 +36,6 @@ func (c *client) UpdateCardByID(ctx context.Context, id int64, input *domain.Upd
 		}
 	}
 
-	sets = append(sets, "updated_at = NOW()")
-
 	if len(sets) > 0 {
 		query = query + " SET " + strings.Join(sets, ", ")
 	}
@@ -46,6 +45,8 @@ func (c *client) UpdateCardByID(ctx context.Context, id int64, input *domain.Upd
 	if err != nil {
 		return nil, errutil.Wrap(err)
 	}
+
+	logger.FromCtx(ctx).Debug(ctx, "UpdateCardByID query: %s, args: %v", query, args)
 
 	var card model.Card
 	if err = e.QueryRowxContext(ctx, query, args...).StructScan(&card); err != nil {
